01f1c8ad73af3f993d617c803a8a430ebf079485
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-04-06 09:45:37 +0700

    lifecycle activity

--- app/src/main/java/com/example/ridhoapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/ridhoapps/pertemuan_4/FourthActivity.kt
@@ -11,6 +11,8 @@ import com.example.ridhoapps.MainActivity
 import com.example.ridhoapps.R
 import com.example.ridhoapps.databinding.ActivityFourthBinding
 import com.example.ridhoapps.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 
 
 class FourthActivity : AppCompatActivity() {
@@ -31,6 +33,28 @@ class FourthActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+
+        }
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup"){
+                    Log.e("Info Snackbar","Snackbar ditutup")
+                }
+                .show()
+        }
+        binding.btnShowAlertDialog.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
         }
 
         val name = intent.getStringExtra("name")
