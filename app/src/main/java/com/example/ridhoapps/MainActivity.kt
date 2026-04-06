f3346dd8d8b5e913b0e068933352aee1d93e2772
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-04-06 08:05:07 +0700

    pertemuan 4 penambahan warming up ,intent getputextra dan getStringextra

--- app/src/main/java/com/example/ridhoapps/MainActivity.kt
+++ app/src/main/java/com/example/ridhoapps/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.ridhoapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ridhoapps.databinding.ActivityFourthBinding
+import com.example.ridhoapps.databinding.ActivityMainBinding
+import com.example.ridhoapps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Inisialisasi komponen
+//        val noTujuan: EditText = findViewById(R.id.InputNoTujuan)
+//        val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirimkan.setOnClickListener {
+
+            val intent = Intent(this, FourthActivity::class.java)
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
+
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/ridhoapps/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ridhoapps.pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ridhoapps.R
+
+class FourthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fourth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+    }
+}
