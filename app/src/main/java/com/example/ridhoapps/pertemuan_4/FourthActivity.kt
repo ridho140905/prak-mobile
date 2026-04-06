3c079432aa5e72ac52b9d72338635a69163e77ed
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-04-06 08:40:25 +0700

    pertemuan 4 penambahan intent kembali

--- app/src/main/java/com/example/ridhoapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/ridhoapps/pertemuan_4/FourthActivity.kt
@@ -1,26 +1,51 @@
 package com.example.ridhoapps.pertemuan_4
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ridhoapps.MainActivity
 import com.example.ridhoapps.R
+import com.example.ridhoapps.databinding.ActivityFourthBinding
+import com.example.ridhoapps.databinding.ActivityMainBinding
+
 
 class FourthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFourthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_fourth)
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.btnKembali.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         val name = intent.getStringExtra("name")
         val from = intent.getStringExtra("from")
         val age = intent.getIntExtra("age",0)
         Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+        Log.e("onCreate", "FourthActivity dibuat pertama kali")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart", "onStart: FourthActivity terlihat di layar")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy", "FourthActivity dihapus dari stack")
     }
 }
