4d38cf993401f1ce23bd57e88db9eed4a7c0d4a0
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-04-13 23:51:24 +0700

    Materi 2 Option Menu

--- app/src/main/java/com/example/ridhoapps/pertemuan_5/FifthActivity.kt
+++ app/src/main/java/com/example/ridhoapps/pertemuan_5/FifthActivity.kt
@@ -1,8 +1,11 @@
 package com.example.ridhoapps.pertemuan_5
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.ridhoapps.R
 import com.example.ridhoapps.databinding.ActivityFifthBinding
 
 class FifthActivity : AppCompatActivity() {
@@ -23,10 +26,26 @@ class FifthActivity : AppCompatActivity() {
             setDisplayShowHomeEnabled(true)
         }
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            R.id.action_search -> {
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
                 true
             }
             else -> super.onOptionsItemSelected(item)
