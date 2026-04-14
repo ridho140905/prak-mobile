524f57af708f622202a4538bf1388a134f10d5b8
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-04-14 16:10:22 +0700

    Modifikasi Materi 1 Toolbar

--- app/src/main/java/com/example/ridhoapps/pertemuan_5/FifthActivity.kt
+++ app/src/main/java/com/example/ridhoapps/pertemuan_5/FifthActivity.kt
@@ -19,15 +19,17 @@ class FifthActivity : AppCompatActivity() {
         binding = ActivityFifthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Mengaktifkan Toolbar custom
         setSupportActionBar(binding.toolbar)
         supportActionBar?.apply {
-            title = "Activity Fifth"
-            subtitle = "Ini adalah subtitle"
+            // Pengaturan title dan subtitle ditiadakan di sini karena
+            // sudah diatur otomatis oleh CollapsingToolbarLayout di XML
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
             setHomeAsUpIndicator(R.drawable.ic_arrow_back)
         }
 
+        // Aksi ketika tombol WebView ditekan
         binding.btnWebView.setOnClickListener {
             // Berpindah dari FifthActivity ke WebViewActivity
             val intentPindah = Intent(this@FifthActivity, WebViewActivity::class.java)
@@ -35,21 +37,26 @@ class FifthActivity : AppCompatActivity() {
         }
     }
 
+    // Menampilkan menu di kanan atas Toolbar (Search & Settings)
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return true
     }
 
+    // Mengatur aksi ketika tombol/menu di Toolbar ditekan
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+            // Jika tombol panah kiri (back) ditekan
             android.R.id.home -> {
                 onBackPressedDispatcher.onBackPressed()
                 true
             }
+            // Jika icon search ditekan
             R.id.action_search -> {
                 Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show()
                 true
             }
+            // Jika icon settings ditekan
             R.id.action_settings -> {
                 Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
                 true
