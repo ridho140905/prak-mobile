968bb2c241b7470e35c39f4dbbce5ba5db62d71f
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-06-08 08:27:18 +0700

    penerapan Generate QR Code p13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    // library Zxing
+    implementation("com.google.zxing:core:3.5.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
